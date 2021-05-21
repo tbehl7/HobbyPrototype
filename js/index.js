e041aabe9d3664c963f6467b5c782b8bc5c4da3d
tbehl7 <62971454+tbehl7@users.noreply.github.com>
2021-05-20 20:24:14 -0400

    HW21-HobbyPrototypeSetup

---
+++ js/index.js
@@ -0,0 +1,26 @@
+function fade(){
+    $(".preloader").fadeOut("slowly");
+}
+
+setTimeout(fade, 3000);
+
+$(".toggle").click(function(){
+  $(this).find(".toggleContainer").toggleClass("active");
+  $(this).siblings(".toggle").find(".toggleContainer").removeClass("active");
+});
+    
+
+$(".searchButton").on("click", function(){
+    $(".searchButton").toggleClass("active");
+    if ($(".searchButton").hasClass("active")){
+            $(".searchBar").css("height", "100vh");
+            $("#searchForm").css("opacity", "1");
+            $(".searchIcon").removeClass("fa-search").addClass("fa-times");
+        }
+    
+    else {
+        $(".searchBar").css("height", "0vh");
+        $("#searchForm").css("opacity", "0");
+        $(".searchIcon").removeClass("fa-times").addClass("fa-search");
+    }
+});
